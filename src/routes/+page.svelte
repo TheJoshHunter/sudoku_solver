25ce6eb19e9892e007e8d4c12b5bd782988e3e1c
Josh Hunter <jshhntr2001@gmail.com>
2024-01-03 00:15:45 -0600

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+    import '../../node_modules/bootstrap/dist/js/bootstrap.js';
+
+</script>
+
+<svelte:head>
+    <title>Sudoku Solver</title>
+</svelte:head>
+
+<button type="button" class="btn btn-primary">Primary</button>
