b075c7bb265a68a617de8ff2e26df62d249aa5c2
Josh Hunter <jshhntr2001@gmail.com>
2024-01-20 18:18:38 -0600

    Remove unsolvable test case and check for completion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,17 +33,17 @@
             ],
         },
         {
-            name: "easy2",
+            name: "test case",
             board: [
-                [9, 2, 6, 7, 8, 1, 0, 0, 3],
-                [8, 0, 0, 4, 3, 9, 6, 2, 0],
-                [4, 0, 3, 0, 2, 6, 0, 0, 0],
-                [0, 8, 0, 0, 0, 0, 7, 0, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 5],
-                [0, 3, 4, 0, 5, 8, 0, 6, 0],
-                [1, 0, 7, 0, 6, 0, 4, 5, 2],
-                [0, 6, 0, 0, 0, 7, 0, 0, 0],
-                [0, 0, 2, 1, 0, 5, 0, 8, 0],
+                [3, 0, 6, 5, 0, 8, 4, 0, 0],
+                [5, 2, 0, 0, 0, 0, 0, 0, 0],
+                [0, 8, 7, 0, 0, 0, 0, 3, 1],
+                [0, 0, 3, 0, 1, 0, 0, 8, 0],
+                [9, 0, 0, 8, 6, 3, 0, 0, 5],
+                [0, 5, 0, 0, 9, 0, 6, 0, 0],
+                [1, 3, 0, 0, 0, 0, 2, 5, 0],
+                [0, 0, 0, 0, 0, 0, 0, 7, 4],
+                [0, 0, 5, 2, 0, 6, 3, 0, 0],
             ],
         },
     ];
@@ -65,6 +65,7 @@
 
     let board_is_solvable: boolean = false;
     let problem_text: string = "";
+    let success_text: string = "";
 
     let solving: boolean = false;
     let validating: boolean = false;
@@ -79,15 +80,44 @@
         solving = true;
         invoke("solve", { board })
             .then((response) => {
-                // set the board to the response
-                board = response as number[][];
-                solving = false;
+                if (response === board) {
+                    // the board was not changed, so it is not solvable
+                    problem_text =
+                        "For some reason, the board was not changed, it probably is not solvable";
+                    board_is_solvable = false;
+                    success_text = "";
+                } else if (has_zeros(response as number[][])) {
+                    // the board was changed, but it still has zeros, so it is not solvable
+                    problem_text =
+                        "The board was changed, but it still has zeros, so it is not solvable";
+                    board_is_solvable = false;
+                    success_text = "";
+                } else {
+                    // the board was changed, so it was solved
+                    board = response as number[][];
+                    board_is_solvable = true;
+                    problem_text = ""; // make the problem text blank
+                    success_text = "The board was solved!";
+                }
             })
             .catch((error) => {
                 solving = false;
                 // alert the user of the error
                 problem_text = error;
             });
+        solving = false;
+    }
+
+    function has_zeros(board: number[][]): boolean {
+        // check if the board has any zeros
+        for (let row of board) {
+            for (let cell of row) {
+                if (cell == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     async function validate() {
@@ -220,6 +250,12 @@
         </p>
     {/if}
 
+    {#if success_text}
+        <p class="text-success">
+            {success_text}
+        </p>
+    {/if}
+
     <Dropdown class="d-inline-block" group>
         <DropdownToggle caret>Presets</DropdownToggle>
         <DropdownMenu>
@@ -269,6 +305,8 @@
         on:click={() => {
             board = initial_board;
             board_is_solvable = false;
+            problem_text = "";
+            success_text = "";
         }}
     >
         Reset
@@ -282,8 +320,10 @@
 </div>
 
 <pre>
+    Debug:
     board_is_solvable: {board_is_solvable}
     solving: {solving}
     validating: {validating}
-    board: {JSON.stringify(board)}
+    problem_text: {problem_text}
+    success_text: {success_text}
 </pre>
